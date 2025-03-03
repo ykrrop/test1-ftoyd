74696994ce980a9eab4b96475ef4524b891c8abf
ykrrop <kdkropotin@gmail.com>
2025-03-03 15:05:16 +0300

    fix: error message

--- components/ErrorMessage.jsx
+++ components/ErrorMessage.jsx
@@ -1,13 +1,14 @@
+import Image from "next/image";
 import React from "react";
 
 const ErrorMessage = ({ message }) => {
   return (
     <div
-      className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded mb-4"
+      className="flex gap-2 p-4 items-center justify-center bg-[#0F1318] rounded h-[56px]"
       role="alert"
     >
-      <strong className="font-bold">Ошибка! </strong>
-      <span>{message}</span>
+      <Image src="/img/error.svg" alt="Error icon" width={20} height={20} />
+      <p>{message}</p>
     </div>
   );
 };
--- components/MatchTracker.jsx
+++ components/MatchTracker.jsx
@@ -20,32 +20,33 @@ const MatchTracker = () => {
     <div className="p-[42px]">
       <div className="flex justify-between items-center">
         <h1 className="text-4xl font-bold">Match Tracker</h1>
-        <button
-          onClick={() => refresh()}
-          className="bg-[#EB0237] text-white font-bold flex items-center justify-center gap-[10px] w-[204px] h-[56px] rounded-[4px] p-[16px] hover:bg-red-600 transition-colors duration-200 disabled:opacity-50 disabled:cursor-not-allowed"
-          disabled={isValidating}
-          aria-label="Обновление данных матчей"
-        >
-          {isValidating ? "Обновление..." : "Обновить"}
-          <Image
-            src="/img/refresh.svg"
-            alt="Refresh icon"
-            width={20}
-            height={20}
-          />
-        </button>
+        <div className="flex items-center gap-2">
+          {!isLoading && isError && (
+            <ErrorMessage message="Ошибка: не удалось загрузить данные матчей" />
+          )}
+          <button
+            onClick={() => refresh()}
+            className="bg-[#EB0237] text-white font-bold flex items-center justify-center gap-[10px] w-[204px] h-[56px] rounded-[4px] p-[16px] hover:bg-red-600 transition-colors duration-200 disabled:opacity-50 disabled:cursor-not-allowed"
+            disabled={isValidating}
+            aria-label="Обновление данных матчей"
+          >
+            {isValidating ? "Обновление..." : "Обновить"}
+            <Image
+              src="/img/refresh.svg"
+              alt="Refresh icon"
+              width={20}
+              height={20}
+            />
+          </button>
+        </div>
       </div>
 
       {isLoading && (
         <div className="space-y-4">
-          {[...Array(3)].map((_, index) => (
+          {[...Array(6)].map((_, index) => (
             <MatchSkeleton key={index} />
           ))}
         </div>
-      )}
-
-      {!isLoading && isError && (
-        <ErrorMessage message="Ошибка: не удалось загрузить данные матчей" />
       )}
 
       {!isLoading && !isError && matches.length === 0 && (
